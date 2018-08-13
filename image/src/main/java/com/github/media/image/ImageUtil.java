4f78f8e7680184d0736a871214b0e35f15acc4d9
loufeng <990507202@qq.com>
2018-08-13 10:33:34 +0800

    first commit

---
+++ image/src/main/java/com/github/media/image/ImageUtil.java
@@ -0,0 +1,12 @@
+package com.github.media.image;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author loufeng
+ * Date: 2018/8/13 上午10:28.
+ */
+public class ImageUtil {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ImageUtil.class);
+}
---
+++ phantom/src/main/java/com/github/media/phantom/DriverFactory.java
@@ -0,0 +1,12 @@
+package com.github.media.phantom;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author loufeng
+ * Date: 2018/8/13 上午10:29.
+ */
+public class DriverFactory {
+    private static final Logger LOGGER = LoggerFactory.getLogger(DriverFactory.class);
+}
